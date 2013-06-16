7b1286b353e094da5d37b046e1c4fbbf66256455
junhee cho <junhee.cho@lge.com>
2013-06-16 13:34:38 +0900

    modified types in order to disable unexpected state at type level. adds a property check for flagged cell ignores check event

--- MineSweeper.hs
+++ MineSweeper.hs
@@ -1,14 +1,31 @@
 module MineSweeper where
 
-data Cell = Adj Int | Mine | Clear deriving Show 
-data CellState c = Unchecked (Marked c) | Checked c | Boomed deriving Show
-data Marked c = Nomark c | Flag c | Question c deriving Show
-data Event = Mark | Check deriving Show
+newtype CleanCell = CleanCell { adjMine :: Int }  deriving (Show, Eq)
+data MineCell = MineCell deriving (Show, Eq)
+type Cell = Either MineCell CleanCell
+
+-- disables 'Checked MineCell' at type level which corresponds to 'Boomed' type
+data CellState = Unchecked (Marked Cell) | Checked CleanCell | Boomed deriving (Show, Eq)
+data Marked c = Nomark c | Flag c | Question c deriving (Show, Eq)
+data Event = Mark | Check deriving (Show, Eq)
 
 getCell :: Marked c -> c
 getCell (Nomark c) = c
 getCell (Flag c) = c
 getCell (Question c) = c
+
+nextState :: CellState -> Event -> CellState
+nextState cs Check = case cs of
+  -- at first, I've missed this case: Flagged Cell should not receive Check event.
+  Unchecked (Flag c) -> cs
+  Unchecked mc -> case getCell mc of
+                  Left MineCell -> Boomed
+                  Right cc -> Checked cc
+  otherwise -> cs
+
+nextState cs Mark = case cs of
+  Unchecked mc -> Unchecked (nextMark mc)
+  otherwise -> cs
 
 nextMark :: Marked c -> Marked c
 nextMark mc = case mc of
@@ -16,21 +33,3 @@ nextMark mc = case mc of
   Flag c -> Question c
   Question c -> Nomark c
 
-nextState :: CellState Cell -> Event -> CellState Cell
-nextState cs Mark = case cs of
-  Unchecked mc -> Unchecked (nextMark mc)
-  otherwise -> cs
-
-nextState cs Check
-  | hasMine cs = Boomed
-  | otherwise = check cs 
-
-hasMine (Unchecked mc) = case getCell mc of
-  Mine -> True
-  otherwise -> False
-
-check :: CellState c -> CellState c
-check cs = case cs of
-  (Unchecked mc) -> Checked (getCell mc)
-  otherwise -> cs
- 
--- tests/Properties.hs
+++ tests/Properties.hs
@@ -2,12 +2,33 @@ module Properties (properties) where
 
 import Test.QuickCheck
 import Test.Framework.Providers.QuickCheck2 (testProperty)
-import Data.Char
+import MineSweeper
 
-prop_nothing :: String -> Bool
-prop_nothing = \_ -> True
+instance Arbitrary Event where
+  arbitrary = elements [Mark, Check]
+
+instance Arbitrary CleanCell where
+  arbitrary = do
+    numAdjacentMine <- elements [0..8]
+    return $ CleanCell numAdjacentMine
+
+instance Arbitrary MineCell where
+  arbitrary = elements [MineCell]
+
+instance Arbitrary CellState where
+  arbitrary = undefined
+
+isUncheckedCell :: CellState -> Bool
+isUncheckedCell cs = case cs of
+  Unchecked mc -> True
+  otherwise -> False
+
+prop_flagged_cell_does_not_go_to_checked_state :: Cell -> Event -> Bool
+prop_flagged_cell_does_not_go_to_checked_state cell evt = 
+  let flagged = Unchecked (Flag cell)
+  in isUncheckedCell $ nextState flagged evt
 
 --properties :: [Test]
 properties =
-  [ testProperty "dumb" prop_nothing
+  [ testProperty "flag_guards_events" prop_flagged_cell_does_not_go_to_checked_state
   ]
